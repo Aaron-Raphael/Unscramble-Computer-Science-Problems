9fe364b6af67c597462755278ba96886aec4234b
Aaron Raphael H J <aaronraphaelhj@gmail.com>
2020-06-03 20:29:02 +0530

    render: Add final render to code

--- Task2.py
+++ Task2.py
@@ -3,6 +3,8 @@ Read file into texts and calls.
 It's ok if you don't understand how to read files
 """
 import csv
+"""
+
 with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
@@ -11,7 +13,6 @@ with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
-"""
 TASK 2: Which telephone number spent the longest time on the phone
 during the period? Don't forget that time spent answering a call is
 also time spent on the phone.
@@ -36,5 +37,4 @@ with open('calls.csv', 'r') as call_rec:
 # Find phone number of maximum call duration
 maxCallNum = max(call_duration, key= lambda x: call_duration[x])
 
-# Print output
 print(f'\n{maxCallNum} spent the longest time, {call_duration[maxCallNum]} seconds, on the phone during September 2016.')
