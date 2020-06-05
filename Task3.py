a4b3e667bc2a2685023de58bc89aea5184649004
Aaron Raphael H J <aaronraphaelhj@gmail.com>
2020-06-05 23:03:12 +0530

    Final commit

--- Task3.py
+++ Task3.py
@@ -3,6 +3,7 @@ Read file into texts and calls.
 It's ok if you don't understand how to read files.
 """
 import csv
+"""
 
 with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
@@ -12,12 +13,10 @@ with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
-"""
 TASK 3:
-(080) is the area code for fixed line telephones in Bangalore.
+(080) is the area code for fixed line teleph_numbers in Bangalore.
 Fixed line numbers include parentheses, so Bangalore numbers
 have the form (080)xxxxxxx.)
-
 Part A: Find all of the area codes and mobile prefixes called by people
 in Bangalore.
  - Fixed lines start with an area code enclosed in brackets. The area
@@ -27,22 +26,20 @@ in Bangalore.
    is its first four digits, and they always start with 7, 8 or 9.
  - Telemarketers' numbers have no parentheses or space, but they start
    with the area code 140.
-
 Print the answer as part of a message:
 "The numbers called by people in Bangalore have codes:"
  <list of codes>
 The list of codes should be print out one per line in lexicographic order with no duplicates.
-
 Part B: What percentage of calls from fixed lines in Bangalore are made
 to fixed lines also in Bangalore? In other words, of all the calls made
 from a number starting with "(080)", what percentage of these calls
 were made to a number also starting with "(080)"?
-
 Print the answer as a part of a message::
 "<percentage> percent of calls from fixed lines in Bangalore are calls
 to other fixed lines in Bangalore."
 The percentage should have 2 decimal digits
 """
+
 def number_type(ph_number):
     if ph_number[:5] == '(080)':
         return 'bangalore'
@@ -76,6 +73,8 @@ def area_code(calls):
     
     return codes
 
+
+
 def from_to_percent(calls):
     from_count = 0         
     to_count = 0
@@ -94,6 +93,7 @@ def from_to_percent(calls):
     answer = to_count / from_count * 100
     percent = round(answer, 2)
     return percent
+
 
 with open('calls.csv', 'r') as call_rec:
     call_reader = csv.reader(call_rec)
